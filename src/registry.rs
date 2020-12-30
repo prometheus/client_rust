6ed7d91fdd2f0101fd9f108ae4eb3b76504b6820
Max Inden <mail@max-inden.de>
2020-12-30 11:01:58 +0100

    src/registry: Allow creating prefixed sub registries

--- src/registry.rs
+++ src/registry.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 /// A metric registry to register metrics with, later on passed to an encoder
 /// collecting samples of each metric by iterating all metrics in the registry.
 ///
@@ -34,22 +36,108 @@
 /// # assert_eq!(expected, String::from_utf8(buffer).unwrap());
 /// ```
 pub struct Registry<M> {
+    prefix: Option<Prefix>,
     metrics: Vec<(Descriptor, M)>,
+    sub_registries: Vec<Registry<M>>,
 }
 
 impl<M> Registry<M> {
     pub fn new() -> Self {
         Self {
+            prefix: None,
             metrics: Default::default(),
+            sub_registries: vec![],
         }
     }
 
-    pub fn register(&mut self, desc: Descriptor, metric: M) {
+    pub fn sub_registry(&mut self, prefix: &str) -> &mut Self {
+        let prefix = self.prefix.clone().unwrap_or_else(|| String::new().into()) + prefix;
+        let mut sub_registry = Registry::new();
+        sub_registry.prefix = Some(prefix);
+        self.sub_registries.push(sub_registry);
+
+        self.sub_registries
+            .last_mut()
+            .expect("sub_registries not to be empty.")
+    }
+
+    pub fn register(&mut self, mut desc: Descriptor, metric: M) {
+        if let Some(prefix) = &self.prefix {
+            desc.name = (prefix.clone() + desc.name.as_str()).into();
+        }
+
         self.metrics.push((desc, metric));
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = &(Descriptor, M)> {
-        self.metrics.iter()
+    pub fn iter(&self) -> RegistryIterator<M> {
+        let metrics = self.metrics.iter();
+        let sub_registries = self.sub_registries.iter();
+        RegistryIterator {
+            metrics,
+            sub_registries,
+            sub_registry: None,
+        }
+    }
+}
+
+/// Iterator iterating both the metrics registered directly with the registry as
+/// well as all metrics registered with sub-registries.
+pub struct RegistryIterator<'a, M> {
+    metrics: std::slice::Iter<'a, (Descriptor, M)>,
+    sub_registries: std::slice::Iter<'a, Registry<M>>,
+    sub_registry: Option<Box<RegistryIterator<'a, M>>>,
+}
+
+impl<'a, M> Iterator for RegistryIterator<'a, M> {
+    type Item = &'a (Descriptor, M);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(metric) = self.metrics.next() {
+            return Some(metric);
+        }
+
+        loop {
+            if let Some(metric) = self.sub_registry.as_mut().and_then(|i| i.next()) {
+                return Some(metric);
+            }
+
+            self.sub_registry = self.sub_registries.next().map(|r| Box::new(r.iter()));
+
+            if self.sub_registry.is_none() {
+                break;
+            }
+        }
+
+        None
+    }
+}
+
+#[derive(Clone)]
+struct Prefix(String);
+
+impl From<String> for Prefix {
+    fn from(s: String) -> Self {
+        Prefix(s)
+    }
+}
+
+impl From<Prefix> for String {
+    fn from(p: Prefix) -> Self {
+        p.0
+    }
+}
+
+impl Add<&str> for Prefix {
+    type Output = Self;
+    fn add(self, rhs: &str) -> Self::Output {
+        Prefix(self.0 + rhs)
+    }
+}
+
+impl Add<&Prefix> for String {
+    type Output = Self;
+    fn add(self, rhs: &Prefix) -> Self::Output {
+        self + rhs.0.as_str()
     }
 }
 
@@ -98,5 +186,58 @@ mod tests {
         );
 
         assert_eq!(1, registry.iter().count())
+    }
+
+    #[test]
+    fn sub_registry() {
+        let top_level_metric_name = "my_top_level_metric";
+        let mut registry = Registry::<()>::new();
+        registry.register(
+            Descriptor::new("unknown", "some help", top_level_metric_name),
+            (),
+        );
+
+        let prefix_1 = "prefix_1";
+        let prefix_1_metric_name = "my_prefix_1_metric";
+        let sub_registry = registry.sub_registry(prefix_1);
+        sub_registry.register(
+            Descriptor::new("unknown", "some help", prefix_1_metric_name),
+            (),
+        );
+
+        let prefix_1_1 = "prefix_1_1";
+        let prefix_1_1_metric_name = "my_prefix_1_1_metric";
+        let sub_sub_registry = sub_registry.sub_registry(prefix_1_1);
+        sub_sub_registry.register(
+            Descriptor::new("unknown", "some help", prefix_1_1_metric_name),
+            (),
+        );
+
+        let prefix_2 = "prefix_2";
+        let _ = registry.sub_registry(prefix_2);
+
+        let prefix_3 = "prefix_3";
+        let prefix_3_metric_name = "my_prefix_3_metric";
+        let sub_registry = registry.sub_registry(prefix_3);
+        sub_registry.register(
+            Descriptor::new("unknown", "some help", prefix_3_metric_name),
+            (),
+        );
+
+        let mut metric_iter = registry.iter().map(|(desc, _)| desc.name.clone());
+        assert_eq!(Some(top_level_metric_name.to_string()), metric_iter.next());
+        assert_eq!(
+            Some(prefix_1.to_string() + prefix_1_metric_name),
+            metric_iter.next()
+        );
+        assert_eq!(
+            Some(prefix_1.to_string() + prefix_1_1 + prefix_1_1_metric_name),
+            metric_iter.next()
+        );
+        // No metric was registered with prefix 2.
+        assert_eq!(
+            Some(prefix_3.to_string() + prefix_3_metric_name),
+            metric_iter.next()
+        );
     }
 }
