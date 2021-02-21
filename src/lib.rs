78b296320e2b2bbf7cc9e7b1bd3dd137a05eec5d
Max Inden <mail@max-inden.de>
2021-02-21 17:18:40 +0100

    src/lib: Improve example

--- src/lib.rs
+++ src/lib.rs
@@ -11,30 +11,62 @@
 //! # Examples
 //!
 //! ```
-//! # use std::sync::atomic::AtomicU64;
-//! # use open_metrics_client::registry::{Descriptor, Registry};
-//! # use open_metrics_client::metrics::counter::Counter;
+//! # use open_metrics_client::encoding::text::Encode;
 //! # use open_metrics_client::encoding::text::encode;
+//! # use open_metrics_client::metrics::counter::{Atomic, Counter};
+//! # use open_metrics_client::metrics::family::Family;
+//! # use open_metrics_client::registry::{Descriptor, ConvenientRegistry};
+//! # use std::io::Write;
+//! # use std::sync::atomic::AtomicU64;
 //! #
-//! // Create registry and counter and register the latter with the former.
-//! let mut registry = Registry::default();
-//! let counter = Counter::<AtomicU64>::new();
+//! // Create a metric registry.
+//! let mut registry = ConvenientRegistry::default();
+//!
+//! // Define a type representing a metric label set, i.e. a key value pair.
+//! //
+//! // You could as well use `(String, String)` to represent a label set,
+//! // instead of the custom type below.
+//! #[derive(Clone, Hash, PartialEq, Eq, Encode)]
+//! struct Labels {
+//!   // Use your own enum types to represent label values.
+//!   method: Method,
+//!   // Or just a plain string.
+//!   path: String,
+//! };
+//!
+//! #[derive(Clone, Hash, PartialEq, Eq, Encode)]
+//! enum Method {
+//!   GET,
+//!   PUT,
+//! };
+//!
+//! // Create a sample counter metric family utilizing the above custom label
+//! // type, representing the number of HTTP requests received.
+//! let http_requests = Family::<Labels, Counter<AtomicU64>>::default();
+//!
+//! // Register the metric family with the registry.
 //! registry.register(
-//!   "my_counter",
-//!   "This is my counter",
-//!   counter.clone(),
+//!   // With the metric name.
+//!   "http_requests",
+//!   // And the metric help text.
+//!   "Number of HTTP requests received",
+//!   Box::new(http_requests.clone()),
 //! );
 //!
-//! // Record an observation by increasing the counter.
-//! counter.inc();
+//! // Somewhere in your business logic record a single HTTP GET request.
+//! http_requests.get_or_create(
+//!     &Labels { method: Method::GET, path: "/metrics".to_string() }
+//! ).inc();
 //!
-//! // Encode all metrics in the registry in the text format.
+//! // When a monitoring system like Prometheus scrapes the local node, encode
+//! // all metrics in the registry in the text format, and send the encoded
+//! // metrics back.
 //! let mut buffer = vec![];
 //! encode(&mut buffer, &registry).unwrap();
 //!
-//! let expected = "# HELP my_counter This is my counter.\n".to_owned() +
-//!                "# TYPE my_counter counter\n" +
-//!                "my_counter_total 1\n" +
+//! let expected = "# HELP http_requests Number of HTTP requests received.\n".to_owned() +
+//!                "# TYPE http_requests counter\n" +
+//!                "http_requests_total{method=\"GET\",path=\"/metrics\"} 1\n" +
 //!                "# EOF\n";
 //! assert_eq!(expected, String::from_utf8(buffer).unwrap());
 //! ```
