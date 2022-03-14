6fd0693393f18d6d61f961a4983c52eb4d714388
akihito.nakano <sora.akatsuki@gmail.com>
2022-03-15 01:41:20 +0900

    src/encoding/proto: Implement encoding for CounterWithExemplar
    Signed-off-by: ackintosh <sora.akatsuki@gmail.com>

--- src/encoding/proto.rs
+++ src/encoding/proto.rs
@@ -4,8 +4,9 @@ pub mod openmetrics_data_model {
 }
 
 use crate::metrics::counter::Counter;
+use crate::metrics::exemplar::{CounterWithExemplar, Exemplar};
 use crate::metrics::family::{Family, MetricConstructor};
-use crate::metrics::{MetricType, TypedMetric};
+use crate::metrics::{counter, MetricType, TypedMetric};
 use crate::registry::{Registry, Unit};
 use std::ops::Deref;
 
@@ -138,31 +139,36 @@ impl<T: EncodeLabel> EncodeLabel for &[T] {
     }
 }
 
+pub trait EncodeCounterValue {
+    fn encode(&self) -> openmetrics_data_model::counter_value::Total;
+}
+
+impl EncodeCounterValue for u64 {
+    fn encode(&self) -> openmetrics_data_model::counter_value::Total {
+        openmetrics_data_model::counter_value::Total::IntValue(*self)
+    }
+}
+
+impl EncodeCounterValue for f64 {
+    fn encode(&self) -> openmetrics_data_model::counter_value::Total {
+        openmetrics_data_model::counter_value::Total::DoubleValue(*self)
+    }
+}
+
 /////////////////////////////////////////////////////////////////////////////////
 // Counter
 
-impl EncodeMetric for Counter {
+impl<N, A> EncodeMetric for Counter<N, A>
+where
+    N: EncodeCounterValue,
+    A: counter::Atomic<N>,
+{
     fn encode(
         &self,
         labels: Vec<openmetrics_data_model::Label>,
     ) -> Vec<openmetrics_data_model::Metric> {
-        let mut metric = openmetrics_data_model::Metric::default();
+        let mut metric = encode_counter(self.get(), None);
         metric.labels = labels;
-
-        metric.metric_points = {
-            let mut metric_point = openmetrics_data_model::MetricPoint::default();
-            metric_point.value = {
-                let mut counter_value = openmetrics_data_model::CounterValue::default();
-                counter_value.total = Some(openmetrics_data_model::counter_value::Total::IntValue(
-                    self.get(),
-                ));
-                Some(openmetrics_data_model::metric_point::Value::CounterValue(
-                    counter_value,
-                ))
-            };
-
-            vec![metric_point]
-        };
 
         vec![metric]
     }
@@ -170,6 +176,76 @@ impl EncodeMetric for Counter {
     fn metric_type(&self) -> MetricType {
         MetricType::Counter
     }
+}
+
+impl<S, N, A> EncodeMetric for CounterWithExemplar<S, N, A>
+where
+    S: EncodeLabel,
+    N: Clone + EncodeCounterValue,
+    A: counter::Atomic<N>,
+    f64: From<N>,
+{
+    fn encode(
+        &self,
+        labels: Vec<openmetrics_data_model::Label>,
+    ) -> Vec<openmetrics_data_model::Metric> {
+        let (value, exemplar) = self.get();
+
+        let exemplar_proto = if let Some(e) = exemplar.as_ref() {
+            Some(encode_exemplar(e))
+        } else {
+            None
+        };
+
+        let mut metric = encode_counter(value.clone(), exemplar_proto);
+        metric.labels = labels;
+
+        vec![metric]
+    }
+
+    fn metric_type(&self) -> MetricType {
+        MetricType::Counter
+    }
+}
+
+fn encode_counter<N>(
+    value: N,
+    exemplar: Option<openmetrics_data_model::Exemplar>,
+) -> openmetrics_data_model::Metric
+where
+    N: EncodeCounterValue,
+{
+    let mut metric = openmetrics_data_model::Metric::default();
+
+    metric.metric_points = {
+        let mut metric_point = openmetrics_data_model::MetricPoint::default();
+        metric_point.value = {
+            let mut counter_value = openmetrics_data_model::CounterValue::default();
+            counter_value.total = Some(value.encode());
+            counter_value.exemplar = exemplar;
+
+            Some(openmetrics_data_model::metric_point::Value::CounterValue(
+                counter_value,
+            ))
+        };
+
+        vec![metric_point]
+    };
+
+    metric
+}
+
+fn encode_exemplar<S, N>(exemplar: &Exemplar<S, N>) -> openmetrics_data_model::Exemplar
+where
+    N: Clone,
+    S: EncodeLabel,
+    f64: From<N>, // required because Exemplar.value is defined as `double` in protobuf
+{
+    let mut exemplar_proto = openmetrics_data_model::Exemplar::default();
+    exemplar_proto.value = exemplar.value.clone().into();
+    exemplar_proto.label = exemplar.label_set.encode();
+
+    exemplar_proto
 }
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -206,6 +282,7 @@ where
 mod tests {
     use super::*;
     use crate::metrics::counter::Counter;
+    use crate::metrics::exemplar::CounterWithExemplar;
     use crate::metrics::family::Family;
     use crate::registry::Unit;
     use std::borrow::Cow;
@@ -244,6 +321,23 @@ mod tests {
             ])
             .inc();
 
+        println!("{:?}", encode(&registry));
+    }
+
+    #[test]
+    fn encode_counter_with_exemplar() {
+        let mut registry = Registry::default();
+
+        let counter_with_exemplar: CounterWithExemplar<(String, f64), f64> =
+            CounterWithExemplar::default();
+        registry.register_with_unit(
+            "my_counter_with_exemplar",
+            "My counter with exemplar",
+            Unit::Seconds,
+            counter_with_exemplar.clone(),
+        );
+
+        counter_with_exemplar.inc_by(1.0, Some(("user_id".to_string(), 42.0)));
         println!("{:?}", encode(&registry));
     }
 }
