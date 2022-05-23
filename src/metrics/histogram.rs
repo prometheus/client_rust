922523527cd55f44d6b65cce702927d5f37b499a
Max Inden <mail@max-inden.de>
2022-05-23 16:28:48 +0200

    Update src/metrics/histogram.rs

--- src/metrics/histogram.rs
+++ src/metrics/histogram.rs
@@ -21,7 +21,7 @@ use std::sync::{Arc, Mutex, MutexGuard};
 /// Golang implementation might work for you:
 ///
 /// ```
-/// # use prometheus_client::metrics::histogram::Historgam;
+/// # use prometheus_client::metrics::histogram::Histogram;
 /// // Default values from go client(https://github.com/prometheus/client_golang/blob/5d584e2717ef525673736d72cd1d12e304f243d7/prometheus/histogram.go#L68)
 /// let histogram = Histogram::new(IntoIterator::into_iter([
 ///    0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
