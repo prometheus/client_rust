fd9445a4e03f8f8113cf8e03b70d215f2ed9734c
Max Inden <mail@max-inden.de>
2022-12-10 14:10:26 +0100

    examples: Fix call without ref
    Signed-off-by: Max Inden <mail@max-inden.de>

--- examples/custom-metric.rs
+++ examples/custom-metric.rs
@@ -20,7 +20,7 @@ impl EncodeMetric for MyCustomMetric {
         // E.g. every CPU cycle spend in this method delays the response send to
         // the Prometheus server.
 
-        encoder.encode_counter::<(), _, u64>(rand::random::<u64>(), None)
+        encoder.encode_counter::<(), _, u64>(&rand::random::<u64>(), None)
     }
 
     fn metric_type(&self) -> prometheus_client::metrics::MetricType {
