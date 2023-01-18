8270e2e13fabd40aedc8383c3ac051f2b5f9e14c
Lars Strojny <lars@strojny.net>
2023-01-19 00:14:13 +0100

    Replace HashMap with BTreeMap to sort metrics deterministically
    Signed-off-by: Lars Strojny <lars@strojny.net>
    Signed-off-by: Jean-Baptiste Skutnik <jb.skutnik@gmail.com>

--- src/metrics/family.rs
+++ src/metrics/family.rs
@@ -6,7 +6,7 @@ use crate::encoding::{EncodeLabelSet, EncodeMetric, MetricEncoder};
 
 use super::{MetricType, TypedMetric};
 use parking_lot::{MappedRwLockReadGuard, RwLock, RwLockReadGuard, RwLockWriteGuard};
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::sync::Arc;
 
 /// Representation of the OpenMetrics *MetricFamily* data type.
@@ -68,12 +68,12 @@ use std::sync::Arc;
 /// # use std::io::Write;
 /// #
 /// # let mut registry = Registry::default();
-/// #[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
+/// #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, EncodeLabelSet)]
 /// struct Labels {
 ///   method: Method,
 /// };
 ///
-/// #[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelValue)]
+/// #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, EncodeLabelValue)]
 /// enum Method {
 ///   GET,
 ///   PUT,
@@ -99,9 +99,8 @@ use std::sync::Arc;
 /// #                "# EOF\n";
 /// # assert_eq!(expected, buffer);
 /// ```
-// TODO: Consider exposing hash algorithm.
 pub struct Family<S, M, C = fn() -> M> {
-    metrics: Arc<RwLock<HashMap<S, M>>>,
+    metrics: Arc<RwLock<BTreeMap<S, M>>>,
     /// Function that when called constructs a new metric.
     ///
     /// For most metric types this would simply be its [`Default`]
@@ -168,7 +167,7 @@ impl<M, F: Fn() -> M> MetricConstructor<M> for F {
     }
 }
 
-impl<S: Clone + std::hash::Hash + Eq, M: Default> Default for Family<S, M> {
+impl<S: Clone + Eq, M: Default> Default for Family<S, M> {
     fn default() -> Self {
         Self {
             metrics: Arc::new(RwLock::new(Default::default())),
@@ -177,7 +176,7 @@ impl<S: Clone + std::hash::Hash + Eq, M: Default> Default for Family<S, M> {
     }
 }
 
-impl<S: Clone + std::hash::Hash + Eq, M, C> Family<S, M, C> {
+impl<S: Clone + Eq, M, C> Family<S, M, C> {
     /// Create a metric family using a custom constructor to construct new
     /// metrics.
     ///
@@ -207,12 +206,7 @@ impl<S: Clone + std::hash::Hash + Eq, M, C> Family<S, M, C> {
     }
 }
 
-impl<S: Clone + std::hash::Hash + Eq, M: Clone, C: MetricConstructor<M>> Family<S, M, C>
-where
-    S: Clone + std::hash::Hash + Eq,
-    M: Clone,
-    C: MetricConstructor<M>,
-{
+impl<S: Clone + Eq + Ord, M: Clone, C: MetricConstructor<M>> Family<S, M, C> {
     /// Access a metric with the given label set, creating it if one does not yet exist.
     ///
     /// ```
@@ -241,7 +235,7 @@ where
     }
 }
 
-impl<S: Clone + std::hash::Hash + Eq, M, C: MetricConstructor<M>> Family<S, M, C> {
+impl<S: Clone + Eq + Ord, M, C: MetricConstructor<M>> Family<S, M, C> {
     /// Access a metric with the given label set, creating it if one does not
     /// yet exist.
     ///
@@ -341,7 +335,7 @@ impl<S: Clone + std::hash::Hash + Eq, M, C: MetricConstructor<M>> Family<S, M, C
         self.metrics.write().clear()
     }
 
-    pub(crate) fn read(&self) -> RwLockReadGuard<'_, HashMap<S, M>> {
+    pub(crate) fn read(&self) -> RwLockReadGuard<'_, BTreeMap<S, M>> {
         self.metrics.read()
     }
 }
@@ -361,7 +355,7 @@ impl<S, M: TypedMetric, C> TypedMetric for Family<S, M, C> {
 
 impl<S, M, C> EncodeMetric for Family<S, M, C>
 where
-    S: Clone + std::hash::Hash + Eq + EncodeLabelSet,
+    S: Clone + Eq + Ord + EncodeLabelSet,
     M: EncodeMetric + TypedMetric,
     C: MetricConstructor<M>,
 {
