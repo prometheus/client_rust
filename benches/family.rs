a4fb2a09a5d0df8ee1cfd85a9067603e1e752339
Michael Zhalevich <cheshirsky.code@gmail.com>
2023-08-16 16:58:28 +0200

    feat(family): add ability to lookup metric by query type in `Family::get_or_create`
    Signed-off-by: Michael Zhalevich <cheshirsky.code@gmail.com>

--- benches/family.rs
+++ benches/family.rs
@@ -1,6 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use prometheus_client::encoding::EncodeLabelSet;
 use prometheus_client::metrics::counter::Counter;
-use prometheus_client::metrics::family::Family;
+use prometheus_client::metrics::family::{CreateFromEquivalent, Equivalent, Family};
 
 pub fn family(c: &mut Criterion) {
     c.bench_function("counter family with Vec<(String, String)> label set", |b| {
@@ -49,6 +50,79 @@ pub fn family(c: &mut Criterion) {
                 .inc();
         })
     });
+
+    c.bench_function(
+        "counter family with custom type label set and direct lookup",
+        |b| {
+            #[derive(Clone, Eq, Hash, PartialEq, EncodeLabelSet)]
+            struct Labels {
+                method: String,
+                url_path: String,
+                status_code: String,
+            }
+
+            let family = Family::<Labels, Counter>::default();
+
+            b.iter(|| {
+                family
+                    .get_or_create(&Labels {
+                        method: "GET".to_string(),
+                        url_path: "/metrics".to_string(),
+                        status_code: "200".to_string(),
+                    })
+                    .inc();
+            })
+        },
+    );
+
+    c.bench_function(
+        "counter family with custom type label set and equivalent lookup",
+        |b| {
+            #[derive(Clone, Eq, Hash, PartialEq, EncodeLabelSet)]
+            struct Labels {
+                method: String,
+                url_path: String,
+                status_code: String,
+            }
+
+            #[derive(Debug, Eq, Hash, PartialEq)]
+            struct LabelsQ<'a> {
+                method: &'a str,
+                url_path: &'a str,
+                status_code: &'a str,
+            }
+
+            impl CreateFromEquivalent<Labels> for LabelsQ<'_> {
+                fn create(&self) -> Labels {
+                    Labels {
+                        method: self.method.to_string(),
+                        url_path: self.url_path.to_string(),
+                        status_code: self.status_code.to_string(),
+                    }
+                }
+            }
+
+            impl Equivalent<Labels> for LabelsQ<'_> {
+                fn equivalent(&self, key: &Labels) -> bool {
+                    self.method == key.method
+                        && self.url_path == key.url_path
+                        && self.status_code == key.status_code
+                }
+            }
+
+            let family = Family::<Labels, Counter>::default();
+
+            b.iter(|| {
+                family
+                    .get_or_create(&LabelsQ {
+                        method: "GET",
+                        url_path: "/metrics",
+                        status_code: "200",
+                    })
+                    .inc();
+            })
+        },
+    );
 }
 
 criterion_group!(benches, family);
